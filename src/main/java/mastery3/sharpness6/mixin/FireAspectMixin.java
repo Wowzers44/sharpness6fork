de355a02009c764451ae900f5bd2896cf9207e88
Wowzers44 <wowzers44@gmail.com>
2023-03-03 11:31:33 -0600

    Update FireAspectMixin.java

--- src/main/java/mastery3/sharpness6/mixin/FireAspectMixin.java
+++ src/main/java/mastery3/sharpness6/mixin/FireAspectMixin.java
@@ -4,10 +4,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import mastery3.sharpness6.Entry;
 
-@Mixin(net.minecraft.enchantment.EfficiencyEnchantment.class)
+@Mixin(net.minecraft.enchantment.FireAspectEnchantment.class)
 public class EfficiencyMixin {
 	@Overwrite
 	public int getMaxLevel() {
-		return Entry.getInstance().config.efficiency;
+		return Entry.getInstance().config.fireaspect;
 	}
 }
